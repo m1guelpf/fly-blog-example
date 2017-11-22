2960cee75b283d0e7012ad0a6f8c6e5d01d2e9dc
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-11-22 18:49:33 +0100

    PostsController

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
+     * Show posts.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts', ['posts' => Post::all()]);
+    }
+    
+    /**
+     * Show a specific post
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('post')->withPost($post);
+    }
+    
+    /**
+     * Persist post to database
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $post = Auth::user()->posts()->create($request->validate([
+        'title' => 'required|string',
+        'body'  => 'required|string',
+      ]));
+    
+        return redirect()->route('post', $post);
+    }
+}
