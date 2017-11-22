913020d207a0f829b81fed461460693a24625a79
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-11-22 18:54:42 +0100

    Setting up routing

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,20 @@
 |
 */
 
+Route::group(['domain' => '[YOUR_APP_DOMAIN_HERE]'], function () {
+    Route::get('/', 'PostsController@index')->name('index');
+});
+
+Route::group(['domain' => '{domain}'], function () {
+    Route::get('/', 'DomainController@index');
+});
+
 Route::get('/', 'PostsController@index')->name('index');
 
 Auth::routes();
 
 Route::get('posts/{post}', 'PostsController@show')->name('post');
 Route::post('posts', 'PostsController@create')->name('create');
+
+Route::view('domain', 'domain-setup')->middleware('auth')->name('domain-setup');
+Route::post('domain', 'DomainController@create')->middleware('auth');
